ec46c2b71330366f1f7c6f4a35bc1a369215d827
Wahyoe We <wahyu.wied@gmail.com>
2025-06-13 15:32:31 +0700

    fix: register options, perbaikan request dto (EKlaimNewClaimService)

--- AptOnline.Infrastructure/EKlaimContext/EKlaimNewClaimService.cs
+++ AptOnline.Infrastructure/EKlaimContext/EKlaimNewClaimService.cs
@@ -21,9 +21,22 @@ namespace AptOnline.Infrastructure.EKlaimContext
 
         public EKlaimNewClaimDto Execute(EKlaimModel req)
         {
+            var reqObj = new EKlaimNewClaimRequest
+            {
+                metadata = new EKlaimNewClaimRequestMeta { method = "new_claim" },
+                data = new EKlaimNewClaimRequestData
+                {
+                    nomor_kartu = req.PesertaBpjs.PesertaBpjsNo,
+                    nomor_sep = req.Sep.SepNo,
+                    nomor_rm = req.Pasien.PasienId,
+                    nama_pasien = req.Pasien.PasienName,
+                    tgl_lahir = req.Pasien.BirthDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                    gender = req.Pasien.Gender.Value
+                }
+            };
             var isDebugMode = _opt.Debug.Equals("1");
             var endpoint = isDebugMode ? _opt.BaseApiUrl + "?mode=debug" : _opt.BaseApiUrl;
-            var reqBody = JsonConvert.SerializeObject(req);
+            var reqBody = JsonConvert.SerializeObject(reqObj);
             var client = new RestClient(endpoint);
             var request = new RestRequest(Method.POST);
             if (!isDebugMode)
@@ -45,13 +58,35 @@ namespace AptOnline.Infrastructure.EKlaimContext
                     return new EKlaimNewClaimDto(false, result.metadata.message);
                 return new EKlaimNewClaimDto(true, result.response.admission_id);
             }
-            catch 
+            catch
             {
                 throw new Exception(tmpResult);
             }
         }
     }
     #region Dto
+
+    public class EKlaimNewClaimRequest
+    {
+        public EKlaimNewClaimRequestMeta metadata { get; set; }
+        public EKlaimNewClaimRequestData data { get; set; }
+    }
+
+    public class EKlaimNewClaimRequestMeta
+    {
+        public string method { get; set; }
+    }
+
+    public class EKlaimNewClaimRequestData
+    {
+        public string nomor_kartu { get; set; }
+        public string nomor_sep { get; set; }
+        public string nomor_rm { get; set; }
+        public string nama_pasien { get; set; }
+        public string tgl_lahir { get; set; }
+        public string gender { get; set; }
+    }
+
     public class EKlaimNewClaimResponse
     {
         public EKlaimResponseMeta metadata { get; set; }
