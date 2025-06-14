8d4e0b84c4679306e3e37a5076f8f1365c6db21d
Jude <drury_yl@yahoo.com>
2025-06-14 21:12:12 +0700

    feat: Tarif RS EKlaim ✅ Value Object terdiri dari;     🔴 Procedure (Tindakan)     🔴 Jasa Medis     🔴 Penunjang     🔴 Akomodasi     🔴 Obat

---
+++ AptOnline.Domain/EKlaimContext/TarifRsFeature/BillJasaMedisType.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.GuardClauses;
+
+namespace AptOnline.Domain.EKlaimContext.TarifRsFeature;
+
+public record BillJasaMedisType(decimal Konsultasi, decimal TenagaAhli, decimal Keperawatan)
+{
+    public static BillJasaMedisType Create(decimal konsultasi, decimal tenagaAhli, decimal keperawatan)
+    {
+        Guard.Against.Negative(konsultasi, nameof(konsultasi));
+        Guard.Against.Negative(tenagaAhli, nameof(tenagaAhli));
+        Guard.Against.Negative(keperawatan, nameof(keperawatan));
+        
+        return new BillJasaMedisType(konsultasi, tenagaAhli, keperawatan);
+    }
+    public static BillJasaMedisType Default => new BillJasaMedisType(0, 0, 0);
+}
