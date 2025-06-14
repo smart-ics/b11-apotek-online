cca11041df78b4392dbe5b67acf857e96a44cd3c
Jude <drury_yl@yahoo.com>
2025-06-14 21:32:34 +0700

    feat: Covid-19 EKlaim ✅ Value Object yang mendeskripsikan status pasien apakah meninggal karna covid atau bukan.

---
+++ AptOnline.Domain/EKlaimContext/Covid19Feature/Covid19Type.cs
@@ -0,0 +1,17 @@
+﻿namespace AptOnline.Domain.EKlaimContext.Covid19Feature;
+
+public record Covid19Type(
+    bool IsPemulasaranJenazah,
+    bool IsKantongJenazah,
+    bool IsPetiJenazah,
+    bool IsPlastikErat,
+    bool IsDesinfektanJenazah,
+    bool IsMobilJenazah,
+    bool IsDesinfektanMobilJenazah)
+{
+    public bool IsCovid19 => IsPemulasaranJenazah || IsKantongJenazah 
+        || IsPetiJenazah || IsPlastikErat || IsDesinfektanJenazah 
+        || IsMobilJenazah || IsDesinfektanMobilJenazah;
+    
+    public static Covid19Type Default => new(false, false, false, false, false, false, false);
+}
