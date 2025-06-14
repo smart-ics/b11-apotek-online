eac12cc6f22c0f8cc072ded4ef8ce40d81e26e85
druryyl <drury_yl@yahoo.com>
2025-06-14 21:12:53 +0700

    feat: Tarif RS EKlaim (#18)
    ✅ Value Object terdiri dari;
        🔴 Procedure (Tindakan)
        🔴 Jasa Medis
        🔴 Penunjang
        🔴 Akomodasi
        🔴 Obat

---
+++ AptOnline.Domain/EKlaimContext/TarifRsFeature/BillObatType.cs
@@ -0,0 +1,26 @@
+﻿using Ardalis.GuardClauses;
+
+namespace AptOnline.Domain.EKlaimContext.TarifRsFeature;
+
+public record BillObatType(
+    decimal Obat,
+    decimal ObatKronis,
+    decimal ObatKemoterapi,
+    decimal Alkes,
+    decimal Bhp,
+    decimal SewaAlat)
+{
+    public static BillObatType Create(decimal obat, decimal obatKronis, decimal obatKemoterapi, decimal alkes,
+        decimal bhp, decimal sewaAlat)
+    {
+        Guard.Against.Negative(obat, nameof(obat));
+        Guard.Against.Negative(obatKronis, nameof(obatKronis));
+        Guard.Against.Negative(obatKemoterapi, nameof(obatKemoterapi));
+        Guard.Against.Negative(alkes, nameof(alkes));
+        Guard.Against.Negative(bhp, nameof(bhp));
+        Guard.Against.Negative(sewaAlat, nameof(sewaAlat));
+        return new BillObatType(obat, obatKronis, obatKemoterapi, alkes, bhp, sewaAlat);
+    }
+
+    public static BillObatType Default => new BillObatType(0, 0, 0, 0, 0, 0);
+}
---
+++ AptOnline.Domain/EKlaimContext/TarifRsFeature/BillPenunjangType.cs
@@ -0,0 +1,23 @@
+﻿using Ardalis.GuardClauses;
+
+namespace AptOnline.Domain.EKlaimContext.TarifRsFeature;
+
+public record BillPenunjangType(
+    decimal Penunjang,
+    decimal Radiologi,
+    decimal Laborat,
+    decimal BankDarah,
+    decimal Rehabilitasi)
+{
+    public static BillPenunjangType Create(decimal penunjang, decimal radiologi, 
+        decimal laborat, decimal bankDarah, decimal rehabilitasi)
+    {
+        Guard.Against.Negative(penunjang, nameof(penunjang));
+        Guard.Against.Negative(radiologi, nameof(radiologi));
+        Guard.Against.Negative(laborat, nameof(laborat));
+        Guard.Against.Negative(bankDarah, nameof(bankDarah));
+        Guard.Against.Negative(rehabilitasi, nameof(rehabilitasi));
+        return new BillPenunjangType(penunjang, radiologi, laborat, bankDarah, rehabilitasi);
+    }
+    public static BillPenunjangType Default => new BillPenunjangType(0, 0, 0, 0, 0); 
+}
