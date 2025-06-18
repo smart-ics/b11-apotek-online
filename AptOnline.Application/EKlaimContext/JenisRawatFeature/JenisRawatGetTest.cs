0d47e78088f23b79d52b2165070d5b868fb9d8df
Jude <drury_yl@yahoo.com>
2025-06-18 19:05:43 +0700

    ✅ Refactor: Ubah Data Jenis Rawat menggunakan Static Data Object (bukan database)

--- AptOnline.Application/EKlaimContext/JenisRawatFeature/JenisRawatGetTest.cs
+++ AptOnline.Application/EKlaimContext/JenisRawatFeature/JenisRawatGetTest.cs
@@ -1,38 +1,24 @@
-﻿using AptOnline.Domain.EKlaimContext.JenisRawatFeature;
+﻿using AptOnline.Application.EKlaimContext.BayiLahirFeature;
 using FluentAssertions;
-using Moq;
-using Nuna.Lib.PatternHelper;
 using Xunit;
 
 namespace AptOnline.Application.EKlaimContext.JenisRawatFeature;
 
 public class JenisRawatGetTest
 {
-    private readonly JenisRawatGetHandler _sut;
-    private readonly Mock<IJenisRawatDal> _jenisRawatDal;
-
-    public JenisRawatGetTest()
-    {
-        _jenisRawatDal = new Mock<IJenisRawatDal>();
-        _sut = new JenisRawatGetHandler(_jenisRawatDal.Object);
-    }
+    private readonly JenisRawatGet _sut = new();
 
     [Fact]
     public async Task UT1_GivenExistedId_ThenReturnExpected()
     {
-        var expected = new JenisRawatGetResponse("A", "B");
-        _jenisRawatDal.Setup(x => x.GetData(It.IsAny<IJenisRawatKey>()))
-            .Returns(MayBe.From(new JenisRawatType("A", "B")));
-        var actual = await _sut.Handle(new JenisRawatGetQuery("A"), CancellationToken.None);
-        actual.Should().BeEquivalentTo(expected);
+        var actual = await _sut.Handle(new JenisRawatGetQuery("1"), CancellationToken.None);
+        actual.Should().BeEquivalentTo(new JenisRawatGetResponse("1", "Rawat Inap"));
     }
 
     [Fact]
     public async Task UT2_GivenInvalidId_ThenThrowEx()
     {
-        _jenisRawatDal.Setup(x => x.GetData(It.IsAny<IJenisRawatKey>()))
-            .Returns(MayBe.From<JenisRawatType>(null!));
-        var actual = () => _sut.Handle(new JenisRawatGetQuery("A"), CancellationToken.None);
+        var actual = () => _sut.Handle(new JenisRawatGetQuery("4"), CancellationToken.None);
         await actual.Should().ThrowAsync<KeyNotFoundException>();
     }
 }
